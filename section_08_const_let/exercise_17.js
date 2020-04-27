4e2a4e25ea2be1ca4c8824a7492f3d50f14f6636
Jaime Biernaski <>
2020-04-27 13:55:09 -0300

    es6 - const and let

---
+++ section_08_const_let/exercise_17.js
@@ -0,0 +1,12 @@
+/*
+ * Imagine that you are building an application to manage a user's 
+ * Facebook profile. A profile might have a 'name', 'age', and 'dateOfBirth'.
+ * Declare three variables with these same names, making sure to use 'const' 
+ * or 'let' depending on whether you expecti the value to change over time.
+ *
+ */
+ 
+
+const name = 'Jane';
+let age = 33;
+const dateOfBirth = '10/12/1986';
